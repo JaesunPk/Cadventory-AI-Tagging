ce7e79f6b3ab0e10548c7db6ad2e0ccc1792c016
Jaesun Park <jsnpk1213@tamu.edu>
2025-03-09 18:38:01 -0500

    created base header files for ModelTagging

---
+++ src/lib/ModelTagging.h
@@ -0,0 +1,29 @@
+#ifndef MODELTAGGING_H
+#define MODELTAGGING_H
+
+#include <string>
+#include <vector>
+
+/**
+ * @brief main class for handling model tags.
+ * 
+ * This class will generate a list of tags for a model after given a set prompt.abs
+ * The prompt will consist of the file path to the model itself, the title, and the object files that are used to generate the model.
+ * This is known as the model's metadata. The extraction of model data will be through the ModelParser class.
+ * 
+ * How to use:
+ * - simply create and instance of the class and call generateTags(std::string filepath) to generate the tags.
+ */
+class ModelTagging {
+public:
+    ModelTagging();
+    ~ModelTagging();
+
+    void generateTags(std::string filepath);
+    std::vector<std::string> getTags();
+
+private:
+    std::vector<std::string> tags;
+};
+
+#endif // MODELTAGGING_H
